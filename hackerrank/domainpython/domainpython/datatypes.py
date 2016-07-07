be2a0d7b858dfc19229720f0a97d33188fbf8289
amirkogit <amirkomail@gmail.com>
2016-07-07 11:18:58 -0400

    more basic code snippets

---
+++ hackerrank/domainpython/domainpython/datatypes.py
@@ -0,0 +1,3 @@
+# print the name of subdomain
+def print_info():
+    print "Running Subdomain --> Data Types"
--- hackerrank/domainpython/domainpython/introduction.py
+++ hackerrank/domainpython/domainpython/introduction.py
@@ -1,8 +1,11 @@
+from __future__ import division
 import sys
 
-def print_name():
-    print "Subdomain - Introduction"
+# print the name of subdomain
+def print_info():
+    print "Running Subdomain --> Introduction"
     
+# if else demo
 def ifelse_demo():
     print "Enter a number"
     N = int(raw_input().strip())
@@ -16,3 +19,38 @@ def ifelse_demo():
     else:
         print "Not Weird"
 
+
+# reading raw input
+def read_raw_input():
+    s = raw_input()
+    print(s)
+
+# arithmetic operators
+def airthmetic_operators():
+    print "Enter two numbers a and b in a newline."
+    a = int(raw_input())
+    b = int(raw_input())
+    print a + b
+    print a - b
+    print a * b
+
+# python division
+def python_division():
+    print "Enter two numbers a and b in a newline."
+
+    # Enter your code here. Read input from STDIN. Print output to STDOUT
+    a = int(raw_input())
+    b = int(raw_input())
+
+    # print integer division
+    print a // b
+
+    # print float division
+    print a / b
+
+# simple loop demo
+def simple_loop():
+    print "Enter a number n. Will print the squares of numbers between 0 and n"
+    n = int(raw_input())
+    for i in range(0,n):
+        print i * i
